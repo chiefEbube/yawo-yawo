b8319b4fc1246e0bed67ad1db8080be899a7fdc7
chiefEbube <ebubechi.anyanwu@yahoo.com>
2023-09-03 18:00:24 +0100

    added landing page

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,36 @@
-const containerEl = document.getElementById("container")
+const landingContainer = document.getElementById("landing-page-container")
 const signInBtn = document.getElementById("sign-in")
+const getStartedBtn = document.getElementById("get-started-btn")
 
-const getHomeHtml = () => {
+const getFirstLandingPageHtml = () => {
     return `
-        <div>Home sweet home</div>
+    <div class="h-screen grid place-items-center">
+        <div>
+            <img src="./images/logo.png" alt="" class="w-36 h-36">
+            <h1 class="text-lightBg font-extrabold text-2xl mt-4">Yawo Yawo</h1>
+        </div>
+    </div>
+
+    <button class="absolute right-4 bottom-10 underline text-lightBg text-sm cursor-pointer" id="get-started-btn">Get started</button>
     `
 }
-const renderHomeHtml = () =>{
-    containerEl.innerHTML = getHomeHtml()
+
+const renderLandingPage = () => {
+    document.body.style.backgroundColor = "white"
+    landingContainer.innerHTML = getFirstLandingPageHtml()
 }
 
-signInBtn.addEventListener('click', () => {
-    renderHomeHtml()
-})
+getStartedBtn.addEventListener('click', renderLandingPage)
+
+// const getHomeHtml = () => {
+//     return `
+//         <div>Home sweet home</div>
+//     `
+// }
+// const renderHomeHtml = () =>{
+//     containerEl.innerHTML = getHomeHtml()
+// }
+
+// signInBtn.addEventListener('click', () => {
+//     renderHomeHtml()
+// })
---
+++ src/js/landingPageData.js
@@ -0,0 +1,7 @@
+const landingPageDataArray = [
+    {
+        title: 'Discover and Connect',
+        text: 'Explore and connect with travelers worldwide in a vibrant group.',
+        
+    }
+]
