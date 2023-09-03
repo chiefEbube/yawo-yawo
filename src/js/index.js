658d60489ea6539d4ced7f50839b2ed67fa129be
chiefEbube <ebubechi.anyanwu@yahoo.com>
2023-09-03 19:00:50 +0100

    changes

--- src/js/index.js
+++ src/js/index.js
@@ -2,52 +2,50 @@ const landingContainer = document.getElementById("landing-page-container")
 // const signInBtn = document.getElementById("sign-in")
 const getStartedBtn = document.getElementById("get-started-btn")
 
-const Pages = [
+const pages = [
     {
         id: 1,
         title: 'Discover and Connect',
         text: 'Explore and connect with travelers worldwide in a vibrant group.',
-        image: '../images/landing1',
+        image: './images/landing1.png',
     },
 
     {
         id: 2,
         title: 'Share Adventures',
         text: 'Share your moments and inspire others with your travel stories.',
-        image: '../images/landing2',
+        image: './images/landing2.png',
     },
 
     {
         id: 3,
         title: 'Travel Safe Anywhere',
         text: 'Enjoy peace of mind with our safety features, ensuring a secure journey.',
-        image: '../images/landing3',
+        image: './images/landing3.png',
     },
 ]
 
-const getPageHtml(page) {
+const getPageHtml = (page) => {
     console.log("well it works")
 
     return `
     <div class="h-screen grid place-items-center">
     <div>
-        <img src=${image} alt="" class="w-36 h-36">
-        <h1 class="text-lightBg font-extrabold text-2xl mt-4">${title}</h1>
-        <p>${text}</p>
+        <img src="${page.image}" alt="" class="w-36 h-36">
+        <h1 class="text-lightBg font-extrabold text-2xl mt-4">${page.title}</h1>
+        <p>${page.text}</p>
     </div>
 </div>
 
-<button class="absolute right-4 bottom-10 underline text-lightBg text-sm cursor-pointer" id="${id}">Get started</button>`
+<button class="absolute right-4 bottom-10 underline text-lightBg text-sm cursor-pointer" id="${page.id}">Get started</button>`
 }
 
-// const renderLandingPage = () => {
-//     document.body.style.backgroundColor = "white"
-//     // landingContainer.innerHTML = landing1.getPageHtml()
-// }
+const renderLandingPage = () => {
+    document.body.style.backgroundColor = "white"
+    landingContainer.innerHTML = getPageHtml(pages[0])
+}
 
-getStartedBtn.addEventListener('click', function(){
-    console.log("clickeddddddddddddddddddd")
-})
+getStartedBtn.addEventListener('click', renderLandingPage)
 
 // const getHomeHtml = () => {
 //     return `
@@ -61,8 +59,3 @@ getStartedBtn.addEventListener('click', function(){
 // signInBtn.addEventListener('click', () => {
 //     renderHomeHtml()
 // })
-
-
-const landing1 = new Page(landingPageData.landing1)
-// const landing2 = new Page(landingPageData.landing2)
-// const landing3 = new Page(landingPageData.landing3)
