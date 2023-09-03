4fe9133be3121e30021e183b9d31377d89e92616
chiefEbube <ebubechi.anyanwu@yahoo.com>
2023-09-03 20:42:08 +0100

    changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,64 +1,96 @@
-const landingContainer = document.getElementById("landing-page-container")
+const pagesEl = document.getElementById("pages")
 // const signInBtn = document.getElementById("sign-in")
 const getStartedBtn = document.getElementById("get-started-btn")
+const pageOneBtn = document.getElementById("page-one-btn")
+const pageTwoBtn = document.getElementById("page-two-btn")
+const pageThreeBtn = document.getElementById("page-three-btn")
 
 const pages = [
     {
-        id: 1,
+        id: 'page1',
         title: 'Discover and Connect',
         text: 'Explore and connect with travelers worldwide in a vibrant group.',
         image: './images/landing1.png',
     },
 
     {
-        id: 2,
+        id: 'page2',
         title: 'Share Adventures',
         text: 'Share your moments and inspire others with your travel stories.',
         image: './images/landing2.png',
     },
 
     {
-        id: 3,
+        id: 'page3',
         title: 'Travel Safe Anywhere',
         text: 'Enjoy peace of mind with our safety features, ensuring a secure journey.',
         image: './images/landing3.png',
     },
 ]
 
-const getPageHtml = (page) => {
-    console.log("well it works")
-
+const getPageHtml1 = () => {
     return `
-    <div class="container max-w-lg min-w-min relative" id="landing-page-container">
         <div class="h-screen grid place-items-center px-4">
             <div>
-                <img src="${page.image}" alt="">
+                <img src="${pages[0].image}" alt="">
                 <div class="mx-auto text-center">
-                    <h1 class=" text-myBlack font-bold text-2xl mt-4">${page.title}</h1>
-                    <p class="text-grayBg">${page.text}</p>
+                    <h1 class=" text-myBlack font-bold text-2xl mt-4">${pages[0].title}</h1>
+                    <p class="text-grayBg">${pages[0].text}</p>
                 </div>
             </div>
         </div>
+    `
+}
 
-        <button class="absolute right-4 bottom-10 underline text-primaryOrange text-sm cursor-pointer" id="${page.id}">Next</button>
-    </div>`
+const getPageHtml2 = () => {
+    return `
+        <div class="h-screen grid place-items-center px-4">
+            <div>
+                <img src="${pages[1].image}" alt="">
+                <div class="mx-auto text-center">
+                    <h1 class=" text-myBlack font-bold text-2xl mt-4">${pages[1].title}</h1>
+                    <p class="text-grayBg">${pages[1].text}</p>
+                </div>
+            </div>
+        </div>
+    `
+}
+
+const getPageHtml3 = () => {
+    return `
+        <div class="h-screen grid place-items-center px-4">
+            <div>
+                <img src="${pages[2].image}" alt="">
+                <div class="mx-auto text-center">
+                    <h1 class=" text-myBlack font-bold text-2xl mt-4">${pages[2].title}</h1>
+                    <p class="text-grayBg">${pages[2].text}</p>
+                </div>
+            </div>
+        </div>
+    `
 }
 
 const renderLandingPage1 = () => {
     document.body.style.backgroundColor = "white"
-    document.body.innerHTML = getPageHtml(pages[0])
+    pagesEl.innerHTML = getPageHtml1()
+    getStartedBtn.style.visibility = "hidden"
+    pageOneBtn.classList.remove("hidden")
 }
 
 const renderLandingPage2 = () => {
     document.body.style.backgroundColor = "white"
-    document.body.innerHTML = getPageHtml(pages[1])
+    pagesEl.innerHTML = getPageHtml2()
+    pageOneBtn.style.visibility = "hidden"
+    pageTwoBtn.classList.remove("hidden")
 }
 
 const renderLandingPage3 = () => {
     document.body.style.backgroundColor = "white"
-    document.body.innerHTML = getPageHtml(pages[2])
+    pagesEl.innerHTML = getPageHtml3()
+    pageTwoBtn.style.visibility = "hidden"
+    pageThreeBtn.classList.remove("hidden")
 }
 
 getStartedBtn.addEventListener('click', renderLandingPage1)
-document.getElementById('2').addEventListener('click', renderLandingPage2)
-document.getElementById('3').addEventListener('click', renderLandingPage3)
+pageOneBtn.addEventListener('click', renderLandingPage2)
+pageTwoBtn.addEventListener('click', renderLandingPage3)
