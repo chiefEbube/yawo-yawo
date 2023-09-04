69b78c88e4eabd8621dab79bbd0fe28c521f683b
chiefEbube <ebubechi.anyanwu@yahoo.com>
2023-09-04 07:34:44 +0100

    add app component

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,43 @@
-const users = {
-    user1: {
-        fullName: 'Elizabeth Yaroson',
-        username: 'Faithaxx',
+const posts = [
+    {
+        username: "faithax",
+        profilePic: "./images/faithax.svg",
+        text: "Who else has visited the new water park in Abuja that just opened? well, i have and i had so much fun there.",
         likes: 40,
-        replies: 25,
-        place: 'Jos, Plateau State',
-        bio: 'I’m a very positive person, i love to travel and make new friends.',
-        post: 'I have and it was so much fun ! Can’t wait to visit again.',
-        followers: 200,
-        following: 150,
-
+        replies: 35,
+        shares: 10,
+        time: 10
     },
-}
+
+    {
+        username: "okoyeM",
+        profilePic: "./images/faithax.svg",
+        image: "./images/image-post.png",
+        text: "Found a new spot to work !",
+        likes: 92,
+        replies: 43,
+        shares: 13,
+        time: 13
+    },
+
+    {
+        username: "davidade",
+        profilePic: "./images/faithax.svg",
+        text: "Who else has visited the new water park in Abuja that just opened? well, i have and i had so much fun there",
+        likes: 92,
+        replies: 43,
+        shares: 13,
+        time: 13
+    },
+
+    {
+        username: "tundeednot",
+        profilePic: "./images/faithax.svg",
+        image: "./images/image-post-2.png",
+        text: "Found a new spot to work !",
+        likes: 92,
+        replies: 43,
+        shares: 13,
+        time: 13
+    },
+]
